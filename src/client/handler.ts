cb57766a05a23b92d182b0a8cd525d86db15f5b8
SunnyGitGud <wecandothissunny@gmail.com>
2025-11-10 19:56:27 +0530

    implimenting ack and nack queues for failed exchanges with dead letter queue fanout

--- src/client/handler.ts
+++ src/client/handler.ts
@@ -3,21 +3,34 @@ import type {
   GameState,
   PlayingState,
 } from "../internal/gamelogic/gamestate.js";
-import { handleMove } from "../internal/gamelogic/move.js";
+import { handleMove, MoveOutcome } from "../internal/gamelogic/move.js";
 import { handlePause } from "../internal/gamelogic/pause.js";
 
-export function handlerPause(gs: GameState): (ps: PlayingState) => void {
-  return (ps: PlayingState) => {
+type AckType = "Ack" | "NackRequeue" | "NackDiscard"
+
+export function handlerPause(gs: GameState): (ps: PlayingState) => Promise<AckType> {
+  return async (ps: PlayingState): Promise<AckType> => {
     handlePause(gs, ps);
     process.stdout.write("> ");
+    return "Ack"
   };
 }
 
-export function handlerMove(gs: GameState): (as: ArmyMove) => void {
-  return (as: ArmyMove) => {
-    handleMove(gs, as)
-    console.log(`Moved ${as.units.length} units to ${as.toLocation}`);
-    process.stdout.write("> ")
+export function handlerMove(gs: GameState): (as: ArmyMove) => Promise<AckType> {
+  return async (as: ArmyMove): Promise<AckType> => {
+    const move = handleMove(gs, as)
+    if (move === MoveOutcome.Safe || move === MoveOutcome.MakeWar) {
+      console.log(`Moved ${as.units.length} units to ${as.toLocation}`);
+      process.stdout.write("> ")
+      return "Ack"
+    }
+    if (move === MoveOutcome.SamePlayer) {
+      console.log("Nack");
+      process.stdout.write("> ")
+      return "NackDiscard"
+    }
+    console.log(`unexpected move ${move} Nack`)
+    return "NackDiscard"
   }
 }
 
--- src/internal/pubsub/subscribe.ts
+++ src/internal/pubsub/subscribe.ts
@@ -1,10 +1,10 @@
 import amqp, { type Channel } from "amqplib";
-
 export enum SimpleQueueType {
   Durable,
   Transient,
 }
 
+type AckType = "Ack" | "NackRequeue" | "NackDiscard"
 
 export async function declareAndBind(
   conn: amqp.ChannelModel,
@@ -18,7 +18,10 @@ export async function declareAndBind(
   const queue = await ch.assertQueue(queueName, {
     durable: queueType === SimpleQueueType.Durable,
     exclusive: queueType !== SimpleQueueType.Durable,
-    autoDelete: queueType !== SimpleQueueType.Durable
+    autoDelete: queueType !== SimpleQueueType.Durable,
+    arguments: {
+      "x-dead-letter-exchange": "peril_dlx"
+    }
   })
   await ch.bindQueue(queue.queue, exchange, key);
   return [ch, queue]
@@ -30,8 +33,9 @@ export async function subscribeJSON<T>(
   queueName: string,
   key: string,
   queueType: SimpleQueueType,
-  handler: (data: T) => void,
+  handler: (data: T) => Promise<AckType> | AckType,
 ): Promise<void> {
+
   const [ch, queue] = await declareAndBind(
     conn,
     exchange,
@@ -40,7 +44,7 @@ export async function subscribeJSON<T>(
     queueType,
   );
 
-  await ch.consume(queue.queue, function(msg: amqp.ConsumeMessage | null) {
+  await ch.consume(queue.queue, async function(msg: amqp.ConsumeMessage | null) {
     if (!msg) return;
 
     let data: T;
@@ -51,8 +55,19 @@ export async function subscribeJSON<T>(
       return;
     }
 
-    handler(data);
-    ch.ack(msg);
+    const acktype = await handler(data);
+    switch (acktype) {
+      case "Ack":
+        ch.ack(msg);
+        break
+      case "NackRequeue":
+        ch.nack(msg, false, true);
+        break
+      case "NackDiscard":
+        console.log("NackDiscard: sending to DLX");
+        ch.nack(msg, false, false)
+        break
+    }
   });
 }
 
